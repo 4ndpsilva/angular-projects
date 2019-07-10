06155da33e335cfd5760eb668041d495501ced27
André <4ndpsilva@gmail.com>
2019-07-10 07:51:03 -0300

    ajustes na versão do angular

--- consulta-fipe/src/app/modules/consulta/consulta-routing.module.ts
+++ consulta-fipe/src/app/modules/consulta/consulta-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ConsultaDetailComponent } from './consulta-detail/consulta-detail.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: '', component: ConsultaDetailComponent}
 ];
 
 @NgModule({
